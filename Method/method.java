531309ad91d6763dbbd5023287991566dcf87034
Michaeljacobej <michaeljacob6789@gmail.com>
2025-02-14 00:12:14 +0700

    adding lambda insruction

--- Method/method.java
+++ Method/method.java
@@ -19,14 +19,38 @@ class Animal {
         System.out.println("Animal makes a sound");  
     }  
 }  
-class Dog extends Animal {  
-    // Overriding method  
-    @Override  
-    public void sound() {  
-        System.out.println("Dog barks");  
-    }  
-}  
 
-public class method {
-    
+class Dog extends Animal {
+    // Overriding method  
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
 }
+
+interface FuncInterface
+{
+    // An abstract function
+    void abstractFun(int x);
+
+    // A non-abstract (or default) function
+    default void normalFun()
+    {
+       System.out.println("Hello");
+    }
+}
+
+class Test
+{
+    public static void main(String args[])
+    {
+        // lambda expression to implement above
+        // functional interface. This interface
+        // by default implements abstractFun()
+        FuncInterface fobj = (int x)->System.out.println(2*x);
+
+        // This calls above lambda expression and prints 10.
+        fobj.abstractFun(5);
+    }
+}
+
